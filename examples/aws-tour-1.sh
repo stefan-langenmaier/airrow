7331ee26992b32f8e1288d28c57a0dff9295792e
Stefan Langenmaier <stefan.langenmaier@gmail.com>
2021-08-14 09:52:27 +0200

    added AWS Tour-01

---
+++ examples/aws-tour-1.sh
@@ -0,0 +1,56 @@
+#!/bin/bash
+
+# aws wasserwerk am roten tor
+curl -X POST \
+  -F 'meta={"creator":"289ae107-aa5f-4423-9b5d-487e449ece56","location":{"lat":48.36,"lon":10.903234},"status":"🌊0️⃣4️⃣⚒️🔋","accuracy":1,"fileName":"04.txt"}' \
+  -F "file=@aws-tour/04.txt" \
+  https://research.xn--rro-pla.de/upload && echo "ok aws wasserwerk am roten tor"
+
+# herkulesbrunnen
+curl -X POST \
+  -F 'meta={"creator":"289ae107-aa5f-4423-9b5d-487e449ece56","location":{"lat":48.365061,"lon":10.899543},"status":"🌊0️⃣9️⃣⚒️⛲🇬🇷🦸🔱","accuracy":1,"fileName":"09.txt"}' \
+  -F "file=@aws-tour/09.txt" \
+  https://research.xn--rro-pla.de/upload && echo "ok herkulesbrunnen"
+
+# merkurbrunnen
+curl -X POST \
+  -F 'meta={"creator":"289ae107-aa5f-4423-9b5d-487e449ece56","location":{"lat":48.367245,"lon":10.898499},"status":"🌊0️⃣8️⃣⚒️⛲☿️🪐","accuracy":1,"fileName":"08.txt"}' \
+  -F "file=@aws-tour/08.txt" \
+  https://research.xn--rro-pla.de/upload && echo "ok merkurbrunnen"
+
+# augustusbrunnen
+curl -X POST \
+  -F 'meta={"creator":"289ae107-aa5f-4423-9b5d-487e449ece56","location":{"lat":48.369040,"lon":10.897939},"status":"🌊0️⃣7️⃣⚒️⛲👑","accuracy":1,"fileName":"07.txt"}' \
+  -F "file=@aws-tour/07.txt" \
+  https://research.xn--rro-pla.de/upload && echo "ok augustusbrunnen"
+
+# stadtmetzg
+curl -X POST \
+  -F 'meta={"creator":"289ae107-aa5f-4423-9b5d-487e449ece56","location":{"lat":48.369642,"lon":10.899465},"status":"🌊1️⃣0️⃣⚒️🍖🔪🥩","accuracy":1,"fileName":"10.txt"}' \
+  -F "file=@aws-tour/10.txt" \
+  https://research.xn--rro-pla.de/upload && echo "ok stadtmetzg"
+
+# brunnenwerk unteres
+curl -X POST \
+  -F 'meta={"creator":"289ae107-aa5f-4423-9b5d-487e449ece56","location":{"lat":48.372148,"lon":10.900865},"status":"🌊0️⃣5️⃣⚒️🎦","accuracy":1,"fileName":"05.txt"}' \
+  -F "file=@aws-tour/05.txt" \
+  https://research.xn--rro-pla.de/upload && echo "ok brunnenwerk unteres"
+
+# lechkanaele
+curl -X POST \
+  -F 'meta={"creator":"289ae107-aa5f-4423-9b5d-487e449ece56","location":{"lat":48.370236,"lon":10.899932},"status":"🌊0️⃣2️⃣⚒️🌉","accuracy":1,"fileName":"02.txt"}' \
+  -F "file=@aws-tour/02.txt" \
+  https://research.xn--rro-pla.de/upload && echo "ok lechkanaele"
+
+# brunnenwerk vogeltor
+curl -X POST \
+  -F 'meta={"creator":"289ae107-aa5f-4423-9b5d-487e449ece56","location":{"lat":48.366036,"lon":10.903591},"status":"🌊0️⃣6️⃣⚒️🐦","accuracy":1,"fileName":"06.txt"}' \
+  -F "file=@aws-tour/06.txt" \
+  https://research.xn--rro-pla.de/upload && echo "ok brunnenwerk vogeltor"
+
+# holbeinplatz
+curl -X POST \
+  -F 'meta={"creator":"289ae107-aa5f-4423-9b5d-487e449ece56","location":{"lat":48.366898,"lon":10.901409},"status":"🌊0️⃣❌⚒️🌉","accuracy":1,"fileName":"02.txt"}' \
+  -F "file=@aws-tour/02.txt" \
+  https://research.xn--rro-pla.de/upload && echo "ok holbeinplatz"
+
