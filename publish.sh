a1f7350dc840cc67a588cc4ecd636c54df57c874
Stefan Langenmaier <stefan.langenmaier@gmail.com>
2021-10-31 09:21:00 +0100

    update branch publish script

--- publish.sh
+++ publish.sh
@@ -1,8 +1,6 @@
 #!/bin/bash
 
 JAVA_HOME=/usr/lib/jvm/java-11-openjdk-amd64 mvn clean package -Dquarkus.container-image.build=true
-docker tag stefan/backend-app:1.0-SNAPSHOT slangenmaier/airrow:latest
 docker tag stefan/backend-app:1.0-SNAPSHOT slangenmaier/airrow:donumenta-latest
 
-docker push slangenmaier/airrow:latest
 docker push slangenmaier/airrow:donumenta-latest
